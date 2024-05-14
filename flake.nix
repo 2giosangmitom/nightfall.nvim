d009c902330170bc2914b4980f1987b51d391d30
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-14 15:16:46 +0700

    chore(nix): remove neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -2,18 +2,15 @@
   description = "A sublime colorscheme for Neovim, enveloping your coding environment in the tranquil shades of twilight, enhancing focus and fostering creativity.";
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
 
   outputs = {
     nixpkgs,
-    neovim-nightly-overlay,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      overlays = [neovim-nightly-overlay.overlay];
       config = {
         allowUnfree = true;
       };
@@ -23,7 +20,6 @@
       buildInputs = with pkgs; [
         alejandra
         nil
-        neovim-nightly
         stylua
         nodejs_22
         git-cliff
