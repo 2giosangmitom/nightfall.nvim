a49c9b946d8049ad10defa86236d546ef6491ac4
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-26 09:33:03 +0700

    refactor(test): simplify code

--- tests/setup_spec.lua
+++ tests/setup_spec.lua
@@ -1,14 +1,14 @@
-describe("setup", function()
+describe("nightfall.nvim setup", function()
   it("should setup nightfall.nvim without error", function()
-    assert.no_error(function() require("nightfall").setup() end)
+    assert.has_no.errors(function() require("nightfall").setup() end)
   end)
 
   it("should set vim.g.colors_name to the specified flavor", function()
-    local flavors = { "nightfall" }
+    local flavors = require("nightfall").supported_flavors
 
-    for _, v in ipairs(flavors) do
-      vim.cmd.colorscheme(v)
-      assert.equal(v, vim.g.colors_name)
+    for _, flavor in ipairs(flavors) do
+      vim.cmd.colorscheme(flavor)
+      assert.are.equal(flavor, vim.g.colors_name)
     end
   end)
 end)
