7669b99faaa9db874a0eaaee8b309b7588b0b6ad
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-26 09:33:00 +0700

    feat: Add setup script for lazy.nvim and mini.doc

---
+++ scripts/minimal_init.lua
@@ -0,0 +1,15 @@
+local root = vim.fn.fnamemodify(".repro", ":p")
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+  vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
+
+local lazypath = root .. "/plugins/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  print(string.format("Installing lazy.nvim to %s", lazypath))
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", lazypath })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup({
+  { "echasnovski/mini.doc", opts = {}, lazy = false },
+})
