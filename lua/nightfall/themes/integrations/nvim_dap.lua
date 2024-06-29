89e6e59b3dd52448e11296a38ccc0f09c791fa98
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-29 17:23:45 +0700

    docs(nvim_dap): provide a snippet for users

--- lua/nightfall/themes/integrations/nvim_dap.lua
+++ lua/nightfall/themes/integrations/nvim_dap.lua
@@ -1,8 +1,26 @@
 --- Enable `nvim-dap` support by adding the following to your Nightfall
 --- integrations config:
+---
 --- >lua
 ---   nvim_dap = { enabled = true }
 --- <
+---
+--- You must define sign icons to make it work. Here is a snippet for you:
+---
+--- >lua
+---   local signs = {
+---     { name = 'DapBreakpoint', text = '', texthl = 'DapBreakpoint' },
+---     { name = 'DapBreakpointCondition', text = '', texthl = 'DapBreakpointCondition' },
+---     { name = 'DapBreakpointRejected', text = '', texthl = 'DapBreakpointRejected' },
+---     { name = 'DapLogPoint', text = '󰛿', texthl = 'DapLogPoint' },
+---     { name = 'DapStopped', text = '󰁕', texthl = 'DapStopped' },
+---   }
+---
+---   for _, sign in ipairs(signs) do
+---     vim.fn.sign_define(sign.name, { text = sign.text, texthl = sign.texthl })
+---   end
+--- <
+---
 ---@tag nightfall.nvim_dap
 
 local M = {}
