9bafb67645dcb72aafd8dde155efcfee89446369
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-12 10:25:58 +0700

    feat: update neotree highlights

--- lua/nightfall/themes/integrations/neo_tree.lua
+++ lua/nightfall/themes/integrations/neo_tree.lua
@@ -12,26 +12,30 @@ function M.get(colors)
   local Options = require("nightfall").Options
   local active_bg = Options.transparent and colors.none or colors.bg_float
   local inactive_bg = Options.transparent and colors.none or colors.background
+  local utils = require("nightfall.utils")
 
   return {
     NeoTreeNormal = { fg = colors.foreground, bg = active_bg },
     NeoTreeNormalNC = { fg = colors.foreground, bg = active_bg },
-    NeoTreeExpander = { fg = colors.deep_navy },
-    NeoTreeIndentMarker = { fg = colors.deep_navy },
-    NeoTreeRootName = { fg = colors.pink, style = { bold = true } },
+    NeoTreeExpander = { fg = colors.dark_blue },
+    NeoTreeIndentMarker = { fg = colors.dark_blue },
+    NeoTreeRootName = {
+      fg = utils.vary_color({ maron = colors.green, deepernight = colors.nord_blue }, colors.pink),
+      style = { bold = true },
+    },
     NeoTreeSymbolicLinkTarget = { fg = colors.pink },
     NeoTreeGitAdded = { fg = colors.added },
     NeoTreeGitConflict = { fg = colors.red },
     NeoTreeGitDeleted = { fg = colors.red },
-    NeoTreeGitModified = { fg = colors.changed },
+    NeoTreeGitModified = { fg = colors.pale_yellow },
     NeoTreeGitUnstaged = { fg = colors.red },
     NeoTreeGitUntracked = { fg = colors.violet },
     NeoTreeGitStaged = { fg = colors.light_cyan },
     NeoTreeFloatBorder = { link = "FloatBorder" },
     NeoTreeFloatTitle = { link = "FloatTitle" },
-    NeoTreeTitleBar = { fg = colors.black, bg = colors.pink },
+    NeoTreeTitleBar = { fg = colors.black, bg = utils.vary_color({ deepernight = colors.nord_blue }, colors.pink) },
     NeoTreeFileNameOpened = { fg = colors.pink },
-    NeoTreeDimText = { fg = colors.dark_navy },
+    NeoTreeDimText = { link = "Comment" },
     NeoTreeFilterTerm = { fg = colors.light_cyan, style = { bold = true } },
     NeoTreeTabActive = { bg = active_bg, fg = colors.lavender, style = { bold = true } },
     NeoTreeTabInactive = { bg = inactive_bg, fg = colors.deep_navy },
