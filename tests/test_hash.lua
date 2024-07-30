60dbddc67f77ad25786ee8d5ca29c3fa8eabc973
Vo Quang Chien <voquangchien.dev@proton.me>
2024-07-30 21:15:36 +0700

    test: migrate to mini.test

--- tests/test_hash.lua
+++ tests/test_hash.lua
@@ -1,43 +1,44 @@
 local hash_module = require("nightfall.hashing")
+local not_eq, eq = MiniTest.expect.no_equality, MiniTest.expect.equality
 
 describe("hashing module", function()
   it("hashes strings correctly", function()
     local str1 = "hello"
     local str2 = "world"
-    assert.is_not.equal(hash_module.hash(str1), hash_module.hash(str2))
-    assert.are.equal(hash_module.hash(str1), hash_module.hash("hello"))
+    not_eq(hash_module.hash(str1), hash_module.hash(str2))
+    eq(hash_module.hash(str1), hash_module.hash("hello"))
   end)
 
   it("hashes numbers correctly", function()
     local num1 = 123
     local num2 = 456
-    assert.is_not.equal(hash_module.hash(num1), hash_module.hash(num2))
-    assert.are.equal(hash_module.hash(num1), hash_module.hash(123))
+    not_eq(hash_module.hash(num1), hash_module.hash(num2))
+    eq(hash_module.hash(num1), hash_module.hash(123))
   end)
 
   it("hashes booleans correctly", function()
     local bool1 = true
     local bool2 = false
-    assert.is_not.equal(hash_module.hash(bool1), hash_module.hash(bool2))
-    assert.are.equal(hash_module.hash(bool1), hash_module.hash(true))
+    not_eq(hash_module.hash(bool1), hash_module.hash(bool2))
+    eq(hash_module.hash(bool1), hash_module.hash(true))
   end)
 
   it("hashes tables with different keys correctly", function()
     local t1 = { key1 = "value1" }
     local t2 = { key2 = "value2" }
-    assert.is_not.equal(hash_module.hash(t1), hash_module.hash(t2))
+    not_eq(hash_module.hash(t1), hash_module.hash(t2))
   end)
 
   it("hashes tables with same keys but different values correctly", function()
     local t1 = { key = "value1" }
     local t2 = { key = "value2" }
-    assert.is_not.equal(hash_module.hash(t1), hash_module.hash(t2))
+    not_eq(hash_module.hash(t1), hash_module.hash(t2))
   end)
 
   it("is consistent regardless of table order", function()
     local t1 = { key1 = "value1", key2 = "value2" }
     local t2 = { key2 = "value2", key1 = "value1" }
-    assert.are.equal(hash_module.hash(t1), hash_module.hash(t2))
+    eq(hash_module.hash(t1), hash_module.hash(t2))
   end)
 
   it("correctly hashes functions based on nightfall options", function()
@@ -75,6 +76,6 @@ describe("hashing module", function()
       end,
     })
 
-    assert.is_not.equal(hash1, hash2)
+    not_eq(hash1, hash2)
   end)
 end)
