3567b50f4848cbb45f8c0ca0b5fbdee087037438
Vo Quang Chien <voquangchien.dev@proton.me>
2024-09-23 22:04:05 +0700

    feat(integrations): add noice.nvim support

--- lua/nightfall/groups/integrations/noice.lua
+++ lua/nightfall/groups/integrations/noice.lua
@@ -1,10 +1,23 @@
--- TODO: add noice.nvim integration
+--- To enable `noice.nvim` support, add the following to your Nightfall
+--- integration config.
+--- >lua
+---   noice = { enabled = true }
+--- <
+---@tag nightfall.nvim_noice
 local M = {}
 
 ---@param colors NightfallPalette
 ---@private
 function M.get(colors)
-  -- Define highlight here
+  return {
+    NoiceCmdline = { fg = colors.green },
+    NoiceCmdlinePopupBorder = { fg = colors.lavender },
+    NoiceCmdlinePopupTitle = { fg = colors.lavender },
+    NoiceCmdlinePrompt = { fg = colors.peach },
+    NoiceCmdlinePopupBorderLua = { fg = colors.nord_blue },
+    NoiceLspProgressClient = { fg = colors.pink },
+    NoiceLspProgressTitle = { fg = colors.light_grey },
+  }
 end
 
 return M
