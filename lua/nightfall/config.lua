f904466e94573e604ed7efa58a89e0253d366d2b
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-09 09:33:19 +0700

    chore(config): don't expose Options

--- lua/nightfall/config.lua
+++ lua/nightfall/config.lua
@@ -2,7 +2,7 @@ local M = {}
 
 --- Default configuration options.
 ---@class NightfallOptions
-M.Options = {
+local Options = {
   transparent = false,
   terminal_colors = true,
   dim_inactive = true,
@@ -19,8 +19,12 @@ M.Options = {
 ---@param user_config? NightfallOptions
 function M.setup(user_config)
   user_config = user_config or {}
-  if not user_config.default_integrations then M.Options.integrations = {} end
-  M.Options = vim.tbl_deep_extend("keep", user_config, M.Options)
+  if not user_config.default_integrations then Options.integrations = {} end
+  Options = vim.tbl_deep_extend("keep", user_config, Options)
 end
+
+--- Get options
+---@return NightfallOptions
+function M.get_options() return Options end
 
 return M
