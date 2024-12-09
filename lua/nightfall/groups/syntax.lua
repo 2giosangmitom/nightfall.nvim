fd8211419326e359ca56fd2f52e7d01fb46470dc
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-09 09:33:35 +0700

    chore: init syntax group

--- lua/nightfall/groups/syntax.lua
+++ lua/nightfall/groups/syntax.lua
@@ -2,11 +2,6 @@ local M = {}
 
 ---@param colors NightfallPalette
 function M.get(colors)
-  local Options = require("nightfall").Options
-  local utils = require("nightfall.utils")
-
-  return {
-  }
 end
 
 return M
