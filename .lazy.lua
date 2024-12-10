4c9314cf326e9aa97e4892e504c2356856cf58e6
Vo Quang Chien <voquangchien.dev@proton.me>
2024-12-10 22:44:37 +0700

    dev: add lazy local spec for development

---
+++ .lazy.lua
@@ -0,0 +1,25 @@
+---@diagnostic disable: missing-fields
+local augroup = vim.api.nvim_create_augroup("nightfall_dev", { clear = true })
+
+local function reload()
+  for k in pairs(package.loaded) do
+    if k:find("^nightfall") then package.loaded[k] = nil end
+  end
+  require("nightfall").setup({})
+  require("nightfall").compile()
+  vim.cmd.colorscheme(vim.g.colors_name)
+end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  group = augroup,
+  callback = reload,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*/lua/nightfall/**.lua",
+  group = augroup,
+  callback = reload,
+})
+
+return {}
