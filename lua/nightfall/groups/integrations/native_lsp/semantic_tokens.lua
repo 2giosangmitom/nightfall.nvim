8eae81a15e050aa18f9e36ffd229ef044809df2a
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-13 20:55:46 +0700

    feat: update colors

--- lua/nightfall/groups/integrations/native_lsp/semantic_tokens.lua
+++ lua/nightfall/groups/integrations/native_lsp/semantic_tokens.lua
@@ -5,8 +5,6 @@ function M.get(colors)
   return {
     ["@lsp.type.class"] = { fg = colors.magenta },
     ["@lsp.type.macro"] = { link = "Preproc" },
-    ["@lsp.mod.abstract"] = { fg = colors.blue },
-    ["@lsp.mod.static"] = { fg = colors.lime },
   }
 end
 
