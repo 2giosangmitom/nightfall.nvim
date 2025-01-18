1f872730b424da6febcb61124346d6d671bdb0d9
Vo Quang Chien <2giosangmitom@proton.me>
2025-01-18 15:40:37 +0700

    fix(semantic_tokens): use treesitter styles for regular variables

--- lua/nightfall/groups/integrations/native_lsp/semantic_tokens.lua
+++ lua/nightfall/groups/integrations/native_lsp/semantic_tokens.lua
@@ -5,6 +5,7 @@ function M.get(colors)
   return {
     ["@lsp.type.class"] = { fg = colors.magenta },
     ["@lsp.type.macro"] = { link = "Preproc" },
+    ["@lsp.type.variable"] = {}, -- Use treesitter styles for regular variables
   }
 end
 
