959f1c32330b929251d09af4379defc6b55e1f12
Vo Quang Chien <2giosangmitom@proton.me>
2025-02-17 09:24:43 +0700

    feat(snacks): add snacks.picker

--- lua/nightfall/config.lua
+++ lua/nightfall/config.lua
@@ -41,7 +41,7 @@ local default_options = {
     treesitter = { enabled = true, context = true },
     render_markdown = { enabled = true },
     native_lsp = { enabled = true, semantic_tokens = true },
-    snacks = { enabled = true, dashboard = true, indent = true },
+    snacks = { enabled = true, dashboard = true, indent = true, picker = true },
     mini = { enabled = true, icons = true },
     blink = { enabled = true },
     flash = { enabled = true },
