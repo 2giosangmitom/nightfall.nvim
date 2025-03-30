608c11527f40d1df58e617fc9eace25e03ce3446
Vo Quang Chien <voquangchien.dev@proton.me>
2025-03-30 16:51:59 +0700

    test: update test cmd

--- scripts/minit.lua
+++ scripts/minit.lua
@@ -1,5 +1,6 @@
 vim.opt.rtp:prepend(".")
+vim.opt.rtp:prepend("deps/mini.doc")
 vim.opt.rtp:prepend("deps/mini.test")
+
 require("mini.test").setup()
-require("mini.test").run()
-vim.cmd("qa!")
+require("mini.doc").setup()
