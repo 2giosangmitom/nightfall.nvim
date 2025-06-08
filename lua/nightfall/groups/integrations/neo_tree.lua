1a7a5b42270754ecd3753f38e1a9ad70904bd24f
Vo Quang Chien <voquangchien.dev@proton.me>
2025-06-08 14:41:46 +0700

    fix(neo_tree): update highlights
    Updated `NeoTreeTitleBar` highlight group

--- lua/nightfall/groups/integrations/neo_tree.lua
+++ lua/nightfall/groups/integrations/neo_tree.lua
@@ -2,11 +2,16 @@ local M = {}
 
 ---@param colors NightfallPalette
 function M.get(colors)
+  local utils = require("nightfall.utils.colors")
   return {
     NeoTreeFloatTitle = { link = "FloatTitle" },
     NeoTreeFloatBorder = { link = "FloatBorder" },
     NeoTreeDimText = { link = "Comment" },
     NeoTreeIndentMarker = { fg = colors.charcoal },
+    NeoTreeTitleBar = {
+      fg = colors.black,
+      bg = utils.vary_color({ nightfall = colors.purple, ["deeper-night"] = colors.sky }, colors.peach),
+    },
   }
 end
 
